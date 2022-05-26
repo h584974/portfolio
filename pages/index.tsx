fa5dc0796ffd178b8fd25ce6ed451986f8788111
= <=>
2022-05-26 20:24:30 +0200

    More styling and restructuring of view styles and home stylesi nto seperate modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,33 @@
 import Image from 'next/image'
-import Styles from '../styles/Home.module.css'
+import HomeStyles from '../styles/Home.module.css'
+import ViewStyles from '../styles/View.module.css'
 import { useText } from '../utils/hooks'
 
 export default function Home() {
     const Text = useText()
     
     return (
-        <div className={Styles.container}>
-            <div className={Styles.content}>
-                <div className={`${Styles.full} ${Styles.center_text} ${Styles.center_content}`}>
-                    <div className={Styles.front}>
+        <div className={HomeStyles.container}>
+            <div className={ViewStyles.content}>
+                <div className={`${ViewStyles.full} ${ViewStyles.max} ${ViewStyles.center_text} ${ViewStyles.center_content}`}>
+                    <div className={HomeStyles.front}>
                     <h1>{Text.front.name}</h1>
                     <h3>{Text.front.description}</h3>
                     </div>
                 </div>
             </div>
 
-            <div className={`${Styles.content} ${Styles.left}`}>
-                <div className={Styles.big}>
+            <div className={`${ViewStyles.content} ${ViewStyles.left}`}>
+                <div className={ViewStyles.big}>
                     <h1>{Text.intro.title}</h1>
                     <p>{Text.intro.description}</p>
                 </div>
             </div>
 
-            <div className={Styles.content}>
-                <div className={Styles.portrait}>
+            <div className={ViewStyles.break_s} />
+
+            <div className={ViewStyles.content}>
+                <div className={HomeStyles.portrait}>
                     <Image
                         src='/portrait.png'
                         alt='Portrait'
@@ -36,10 +39,30 @@ export default function Home() {
                 </div>
             </div>
 
-            <div className={`${Styles.content} ${Styles.right}`}>
-                <div className={Styles.big}>
-                    <h1>{Text.skills.title}</h1>
-                    <p>{Text.skills.description}</p>
+            <div className={ViewStyles.break_s} />
+
+            <div className={`${ViewStyles.content} ${ViewStyles.right}`}>
+                <div className={ViewStyles.big}>
+                    <div>
+                        <h1>{Text.skills.title}</h1>
+                        <p>{Text.skills.description}</p>
+                    </div>
+                    <div className={ViewStyles.break_s}></div>
+                    <div>
+                        <h3>(Technologies)</h3>
+                        <p>(List of technologies + logos)</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={ViewStyles.break_b} />
+
+            <div className={`${ViewStyles.content} ${ViewStyles.left}`}>
+                <div className={`${ViewStyles.medium} ${ViewStyles.half} ${ViewStyles.center_content}`}>
+                    <p>(Centered Content Half Size)</p>
+                </div>
+                <div className={`${ViewStyles.medium} ${ViewStyles.half} ${ViewStyles.center_content}`}>
+                    <p>(Centered Content Half Size)</p>
                 </div>
             </div>
         </div>
--- utils/text.ts
+++ utils/text.ts
@@ -14,18 +14,20 @@ export const English: Text = {
         title: 'About Me',
 
         description: `Hi! My name is Oliver O\`Loughlin. I am norwegian/british and have 23 years of life experience. 
-        I am a computer engineer, with a bachelor's degree from Western Norway University of Applied Sciences. 
-        My passion is computer technology and programming. I enjoy both back-end and front-end work. Coding is what gives 
-        me the most fulfillment, while I feel less confident when it comes to Visual Design. What gets me to enjoy programming  is 
-        to be able to solve interesting or complex problems. I find it fun to use my own knowlegde in combination with 
-        others' to create elegant solutions. I have learned that it usually doesn't go to plan on the 
-        first attempt, but that I always know more and have learned something after trying. In my free time I like to do hobby 
-        projects, play football, learn about tech, and game.`,
+            I am a computer engineer, with a bachelor's degree from Western Norway University of Applied Sciences. 
+            My passion is computer technology and programming. I enjoy both back-end and front-end work. Coding is what gives 
+            me the most fulfillment, while I feel less confident when it comes to Visual Design. What gets me to enjoy programming  is 
+            to be able to solve interesting or complex problems. I find it fun to use my own knowlegde in combination with 
+            others' to create elegant solutions. I have learned that it usually doesn't go to plan on the 
+            first attempt, but that I always know more and have learned something after trying. In my free time I like to do hobby 
+            projects, play football, learn about tech, and game.`,
     },
 
     skills: {
         title: 'Skills',
-        description: 'JavaScript, Java, .NET etc. etc.',
+        description: `The languages and frameworks I am most confident in are JavaScript/TypeScript, React.JS, Next.JS, Node.JS and Java. 
+            I have expecially found a lot of joy from doing web development with Next.JS, TypeScript and React. This website is made using 
+            these technologies.`,
     },
 }
 
@@ -43,16 +45,18 @@ export const Norsk: Text = {
         title: 'Om Meg',
 
         description: `Hei! Mitt navn er Oliver O\`Loughlin. Jeg er norsk/britisk og har 23 år med livserfaring. Jeg er en dataingeniør, 
-        og har en bachelor fra Høgskulen på Vestlandet. Min lidenskap er datateknologi og programmering. Jeg trives godt med både 
-        back-end og front-end utvikling. For meg er det koding som gir mest glede, mens visuell design føler jeg meg svakere på. Det 
-        som får meg til å like programmering er å kunne løse interessante eller kompliserte problemer. Jeg synes det er gøy å kunne 
-        bruke min egen kunnskap i kombinasjon med andres til å komme frem til elegante løsninger. Jeg har lært at det vanligvis ikke 
-        går etter planen på første forsøk, men at jeg alltid kan mer og har lært noe etter å ha prøvd. På fritiden liker jeg å drive 
-        med hobbyprosjekter, spille fotball, lære om tech, og game.`,
+            og har en bachelor fra Høgskulen på Vestlandet. Min lidenskap er datateknologi og programmering. Jeg trives godt med både 
+            back-end og front-end utvikling. For meg er det koding som gir mest glede, mens visuell design føler jeg meg svakere på. Det 
+            som får meg til å like programmering er å kunne løse interessante eller kompliserte problemer. Jeg synes det er gøy å kunne 
+            bruke min egen kunnskap i kombinasjon med andres til å komme frem til elegante løsninger. Jeg har lært at det vanligvis ikke 
+            går etter planen på første forsøk, men at jeg alltid kan mer og har lært noe etter å ha prøvd. På fritiden liker jeg å drive 
+            med hobbyprosjekter, spille fotball, lære om tech, og game.`,
     },
 
     skills: {
         title: 'Ferdigheter',
-        description: 'JavaScript, Java, .NET etc. etc.',
+        description: `Språkene og rammeverkene jeg føler meg sterkest i er JavaScript/Typescript, React.JS, Next.JS, Node.JS og Java. 
+            Jeg har spesielt hatt mye glede av å drive med web utvikling i Next.JS, TypeScript og React. Denne nettsiden er laget ved 
+            bruk av disse teknologiene.`,
     },
 }
