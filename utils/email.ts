58749e43d1724b2d6ef1b34a658e3788442fa52c
= <=>
2022-06-04 00:37:49 +0200

    Custom email domain on production, test 1

--- utils/email.ts
+++ utils/email.ts
@@ -2,10 +2,11 @@ import Mail from '@sendgrid/mail'
 
 export async function sendEmail(from: string, subject: string, text: string) {
     Mail.setApiKey(process.env.SENDGRID_API_KEY as string)
+    const _from = process.env.NODE_ENV === 'production' ? `${from}@oliveroloughlin.com` : 'oliveroloughlin@hotmail.com'
 
     const data = {
         to: 'oliveroloughlin1@gmail.com',
-        from: 'oliveroloughlin@hotmail.com', //`${from}@oliveroloughlin.com`,
+        from: _from,
         subject,
         text,
         html: text.replace(/\r\n/g, '<br>')
