7e0ffb6b67b78a4d3a92f1f55e73f5bb6978bb46
= <=>
2022-06-05 23:34:22 +0200

    Expanded email utility function to accept optional html argument

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -13,13 +13,20 @@ export default async function Email(req: NextApiRequest, res: NextApiResponse) {
 
             const text = `
                 Name: ${name}\r\n
-                Email Address: ${email}\r\n\r\n
+                Email Address: ${email}\r\n
                 Message:\r\n
                 ${message}
             `
 
+            const html = `
+                <strong>Name: </strong>${name}<br/>
+                <strong>Email Address: </strong>${email}<br/>
+                <strong>Message:</strong><br/>
+                <p>${message}</p>
+            `
+
             try {
-                await sendEmail('contact', 'Portfolio Contact Form Message', text)
+                await sendEmail('contact', 'Portfolio Contact Form Message', text, html)
                 ok(res, 'Email Sent Successfully')
             }
             catch (err) {
--- utils/email.ts
+++ utils/email.ts
@@ -1,14 +1,14 @@
 import Mail from '@sendgrid/mail'
 
-export async function sendEmail(from: string, subject: string, text: string) {
+export async function sendEmail(from: string, subject: string, text: string, html?: string) {
     Mail.setApiKey(process.env.SENDGRID_API_KEY as string)
 
     const data = {
         to: 'oliveroloughlin1@gmail.com',
-        from: `${from}@oliveroloughlin.com`,
+        from: `${from}@oloughlin.no`,
         subject,
         text,
-        html: text.replace(/\r\n/g, '<br>')
+        html: html ?? text.replace(/\r\n/g, '<br>')
     }
 
     await Mail.send(data)
