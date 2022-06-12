4a14e87db5ba821ae3de2ed0cc79cee58ee9ac25
= <=>
2022-06-12 22:40:51 +0200

    Added Carousel component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import ViewStyles from '../styles/View.module.css'
-import Carousel from './Carousel'
+import Carousel from './views/Carousel'
 
 export default function Projects() {
     return (
@@ -11,7 +11,14 @@ export default function Projects() {
             </div>
             <div className={ViewStyles.col_span_4}>
                 <h4>Carousel</h4>
-                
+                <div style={{width: '100%', aspectRatio: '16/9'}}>
+                    <Carousel slideshowSeconds={10}>
+                        <div style={{background: 'blue'}}></div>
+                        <div style={{background: 'yellow'}}></div>
+                        <div style={{background: 'green'}}></div>
+                        <div style={{background: 'red'}}></div>
+                    </Carousel>
+                </div>
             </div>
         </div>
     )
---
+++ components/views/Carousel.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useId, useRef, useState } from 'react'
+import CStyles from '../../styles/Carousel.module.css'
+import IStyles from '../../styles/Icons.module.css'
+
+export default function Carousel({ children, slideshowSeconds }: { children: JSX.Element[], slideshowSeconds?: number }) {
+    if (!children) return <></>
+
+    const [slideWidth, setSlideWidth] = useState<string>('100%')
+    const [currentIndex, setCurrentIndex] = useState<number>(0)
+    const containerRef = useRef<HTMLDivElement>(null)
+    const slidesRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (slideshowSeconds) {
+            const interval = setInterval(next, slideshowSeconds * 1_000)
+            return () => clearInterval(interval)
+        }
+    }, [])
+
+    useEffect( () => {
+        const width = getSlideWithPixels()
+        if (slidesRef.current) slidesRef.current.scrollLeft = currentIndex * (width + .75)
+    }, [currentIndex])
+
+    useEffect( () => {
+        const carousel = containerRef.current
+        if (carousel) new ResizeObserver(updateSlideWidth).observe(carousel)
+    }, [containerRef.current])
+
+    const mainID = useId()
+    const childrenIDs = children.map( (child, index) => {
+        const id: string = `${mainID}-${index}`
+        return {
+            child,
+            id
+        }
+    })
+
+    function next() {
+        setCurrentIndex( index => (index + 1) % children.length )
+    }
+
+    function prev() {
+        setCurrentIndex( index => {
+            let i = index - 1
+            if (i < 0) i = children.length - 1
+            return i
+        } )
+    }
+
+    function updateSlideWidth() {
+        const carousel = containerRef.current
+        const width = carousel ? window.getComputedStyle(carousel).width : '100%'
+        setSlideWidth(width)
+        setCurrentIndex(0)
+    }
+
+    function getSlideWithPixels() {
+        const carousel = containerRef.current
+        const width = carousel ? window.getComputedStyle(carousel).width : '0'
+        return parseInt(width)
+    }
+
+    return (
+        <div className={CStyles.container} ref={containerRef}>
+            <div className={CStyles.track}>
+                <div className={CStyles.slides} ref={slidesRef}>
+                    {childrenIDs.map( ({ child, id }) => <div style={{width: slideWidth}} className={CStyles.slide} id={id} key={`slide-${id}`}>{child}</div> )}
+                </div>
+            </div>
+            <div className={CStyles.nav}>
+                {childrenIDs.map( ({ id }, index) => {
+                    return (
+                        <button 
+                            className={currentIndex == index ? CStyles.nav_selected : ''} 
+                            key={`button-${id}`} 
+                            value={index} 
+                            onClick={(e: any) => setCurrentIndex(e.target.value as number)}>
+                        </button>
+                    )
+                })}
+            </div>
+            <div className={`${CStyles.prev}`}>
+                <button className={`${IStyles.icon} ${IStyles.left}`} onClick={prev}/>
+            </div>
+            <div className={CStyles.next}>
+                <button className={`${IStyles.icon} ${IStyles.right}`} onClick={next}/>
+            </div>
+        </div>
+    )
+}
